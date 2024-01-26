6f7268e91c26a5adff139bbfd8568f200a596cfc
Shehan Chanuka <shehanxdev@gmail.com>
2024-01-26 16:05:15 +0530

    feat: developed the footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Routes from "./Routes";
+import { Footer } from "./components";
 
 function App() {
-  return <Routes />;
+  return (
+    <div>
+      <Routes />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
