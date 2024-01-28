eaab5fb3a4b0705130a706c30bef5caf12f369db
Shehan Chanuka <shehanxdev@gmail.com>
2024-01-28 17:13:23 +0530

    refactor/refactored footer to stick to the bottom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Footer() {
   return (
-    <footer className="mt-5 py-3 bg-dark position-relative w-100 bottom-0">
+    <footer className="py-3 bg-dark position-absolute w-100 bottom-0">
       <div className="d-flex justify-content-center">
         <span className="text-light">Fidenz Academy - 2024</span>
       </div>
